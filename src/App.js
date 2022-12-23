1b30e2aa3fc944db2461c54194a758abcd4fde73
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-12-23 21:41:04 +0100

    commiting initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+    const user = {
+        name: 'Hedy Lamarr',
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90,
+    }
+
+    return (
+      
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+            />
+            <ProductList/>
     </div>
   );
+}
+
+function ProductList()
+{
+    const products = [
+        { title: 'Cabbage', id: 1 },
+        { title: 'Garlic', id: 2 },
+        { title: 'Apple', id: 3 },
+      ];
+    const listItems = products.map(product =>
+        <li key={product.id}>
+          {product.title}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+      );
+      
 }
 
 export default App;
