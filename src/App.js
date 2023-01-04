624bdec0e61ca46c1e6004db3f554d8212dea5e5
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-01-04 22:57:14 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,73 +1,58 @@
 import './App.css';
-import { useState } from "react"
+import { useState } from 'react';
 import ProductTableD from './ProductTable';
-import Profile from "./props/avatar"
+import Profile from './props/avatar';
 import Gallery from './prop-task/prop';
-function App()
-{
-    const [count, setCount] = useState(0)
-    const handleClick = () =>
-    {
-        setCount(count+1)
-    }
-    
-    const user = {
-        name: 'Hedy Lamarr',
-        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-        imageSize: 90,
-    }
+import PackingList from './conditionalRender/list';
+function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
-    return (
-    <div className="App">
-       <h1>{user.name}</h1>
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+
+  return (
+    <div className='App'>
+      <h1>{user.name}</h1>
       <img
-        className="avatar"
+        className='avatar'
         src={user.imageUrl}
         alt={'Photo of ' + user.name}
         style={{
           width: user.imageSize,
-          height: user.imageSize
+          height: user.imageSize,
         }}
-            />
-            <ProductList />
+      />
+      {/* <ProductList />
             <MyBtn count={count} onClick={handleClick} />
-            <MyBtn count={count} onClick={handleClick} />
-            <ProductTableD />
-            
-            <Gallery />
-            
-<Profile/>
+            <MyBtn count={count} onClick={handleClick} /> */}
+      <ProductTableD />
+      <Gallery />
+      <Profile />
+      <PackingList />
     </div>
   );
 }
 
-function ProductList()
-{
-    const products = [
-        { title: 'Cabbage', id: 1 },
-        { title: 'Garlic', id: 2 },
-        { title: 'Apple', id: 3 },
-      ];
-    const listItems = products.map(product =>
-        <li key={product.id}>
-          {product.title}
-        </li>
-    );
-    return (
-        
-        <ul>{listItems}</ul>
-      );
-      
+function ProductList() {
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  const listItems = products.map((product) => (
+    <li key={product.id}>{product.title}</li>
+  ));
+  return <ul>{listItems}</ul>;
 }
 
-function MyBtn({count, onClick})
-{
- 
-    return (
-        <button onClick={onClick}>
-            you have clicked me {count} times
-        </button>
-    )
+function MyBtn({ count, onClick }) {
+  return <button onClick={onClick}>you have clicked me {count} times</button>;
 }
 
 export default App;
