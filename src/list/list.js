31f03c50ff4d0e82d68db56f547f57519c38ca60
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-01-06 00:27:45 +0100

    rendering list

---
+++ src/list/list.js
@@ -0,0 +1,14 @@
+const people = [
+    'Creola Katherine Johnson: mathematician',
+    'Mario José Molina-Pasquel Henríquez: chemist',
+    'Mohammad Abdus Salam: physicist',
+    'Percy Lavon Julian: chemist',
+    'Subrahmanyan Chandrasekhar: astrophysicist'
+  ];
+  
+  export default function List() {
+    const listItems = people.map(person =>
+      <li>{person}</li>
+    );
+    return <ul>{listItems}</ul>;
+  }
