c522f01d7dce3305fe0d4fc6e8b561a4739b8ca3
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-02-16 21:45:31 +0100

    fixed disappearing text

--- src/states/fixdisappearingText.js
+++ src/states/fixdisappearingText.js
@@ -8,33 +8,38 @@ import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
   return (
     <div>
-      <Form />
-      <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+      {showHint && (
+        <p>
+          <i>Hint: Your favorite city?</i>
+              </p>
+      )}
+          <Form />
+          
+          {showHint ? 
+              (  <button
+            onClick={() => {
+              setShowHint(false);
+            }}
+          >
+            Hide hint
+              </button> ):(
+          <button
+                  onClick={() => {
+                    setShowHint(true);
+                  }}
+                >
+                  Show hint
+                </button>         
+          ) 
+        }
     </div>
-  );
+  )
+
 }
 
 function Form() {
   const [text, setText] = useState('');
-  return (
-    <textarea
-      value={text}
-      onChange={e => setText(e.target.value)}
-    />
-  );
+  return <textarea value={text} onChange={(e) => setText(e.target.value)} />;
 }
